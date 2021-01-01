5341907bf4691e05ec097507ad9437b10a749436
extern.jm.reckel <jm.reckel@t-online.de>
2021-01-02 00:42:24 +0100

    Kleinere Änderungen und Anpassungen, Fehlerkorrektur Linux-Installer, Fertigstellung

--- make.sh
+++ make.sh
@@ -29,4 +29,18 @@ echo "Target operating system: $OS"
 # Command line to build the sofware
 #
 
-fpc -MObjFPC -Scghi -Cg -Os3 -l -vewnhibq -Filib/x86_64-linux -Fu/usr/lib/lazarus/components/rtticontrols/lib/x86_64-linux/gtk2 -Fu/usr/lib/lazarus/components/ideintf/units/x86_64-linux/gtk2 -Fu/usr/lib/lazarus/components/synedit/units/x86_64-linux/gtk2 -Fu/usr/lib/lazarus/components/lazcontrols/lib/x86_64-linux/gtk2 -Fu/usr/lib/lazarus/lcl/units/x86_64-linux/gtk2 -Fu/usr/lib/lazarus/lcl/units/x86_64-linux -Fu/usr/lib/lazarus/components/lazutils/lib/x86_64-linux -Fu/usr/lib/lazarus/packager/units/x86_64-linux -Fu. -FUlib/x86_64-linux -FE. -o./cantara -dLCL -dLCLgtk2 Cantara.lpr
+fpc -MObjFPC -Scghi -Cg -Os3 -l -vewnhibq -Filib/$ARCH-$OS -Fu/usr/lib/lazarus/components/rtticontrols/lib/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/components/ideintf/units/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/components/synedit/units/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/components/lazcontrols/lib/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/lcl/units/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/lcl/units/$ARCH-$OS -Fu/usr/lib/lazarus/components/lazutils/lib/$ARCH-$OS -Fu/usr/lib/lazarus/packager/units/$ARCH-$OS -Fu. -FUlib/$ARCH-$OS -FE. -ocantara -dLCL -dLCLgtk2 Cantara.lpr
+
+if [ $? == 0 ]; then
+
+    echo "Kompilierung erfolgreich. Cantara kann nun mit 'sudo make install' installiert werden."
+
+    exit 0
+
+else
+
+    echo "Kompilierung fehlgeschlagen. Möglicherweise ist Lazarus nicht installiert oder der vermutete Pfad ist falsch. In diesem Fall ist die Datei make.sh manuell anzupassen (Zeile 32)."
+
+    echo 1
+
+fi
