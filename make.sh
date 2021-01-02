f09206237518fa47bb379bfd5456bd8c2ee411e4
extern.jm.reckel <jm.reckel@t-online.de>
2021-01-02 14:10:28 +0100

    Überarbeite Make

--- make.sh
+++ make.sh
@@ -2,34 +2,7 @@
 # Detects and parses the architecture
 #
 
-ARCH=$(uname -m)
-
-case "$ARCH" in
-
- "i686") ARCH="i386";;
-
- "i586") ARCH="i386";;
-
- "i486") ARCH="i386";;
-
-esac
-
-echo "Target architecture: $ARCH"
-
-#
-# Detects and parses the OS
-#
-
-OS="linux"
-
-echo "Target operating system: $OS"
-
-
-#
-# Command line to build the sofware
-#
-
-fpc -MObjFPC -Scghi -Cg -Os3 -l -vewnhibq -Filib/$ARCH-$OS -Fu/usr/lib/lazarus/components/rtticontrols/lib/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/components/ideintf/units/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/components/synedit/units/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/components/lazcontrols/lib/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/lcl/units/$ARCH-$OS/gtk2 -Fu/usr/lib/lazarus/lcl/units/$ARCH-$OS -Fu/usr/lib/lazarus/components/lazutils/lib/$ARCH-$OS -Fu/usr/lib/lazarus/packager/units/$ARCH-$OS -Fu. -FUlib/$ARCH-$OS -FE. -ocantara -dLCL -dLCLgtk2 Cantara.lpr
+lazbuild Cantara.lpi
 
 if [ $? == 0 ]; then
 
