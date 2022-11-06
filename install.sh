64f0882ed1c427f8b55e3f4a6279d1049e64a41f
Adrian Freund <adrian@freund.io>
2022-11-06 20:14:59 +0100

    Add metadata and rename files to prepare for flathub release

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
 install cantara /usr/bin/cantara
-install Cantara.png /usr/share/icons/cantara.png
+install app.cantara.Cantara.png /usr/share/icons/app.cantara.Cantara.png
 
-xdg-desktop-menu install reckel-cantara.desktop
+xdg-desktop-menu install app.cantara.Cantara.desktop
 
 install languages/de/cantara.mo /usr/share/locale/de/LC_MESSAGES/cantara.mo
 install languages/zh/cantara.mo /usr/share/locale/zh/LC_MESSAGES/cantara.mo
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
 #
 # Don´t use "rm -rf" in here, because you should only remove the files you created
 #
-rm -f /usr/share/icons/cantara.png
+rm -f /usr/share/icons/app.cantara.Cantara.png
 
 rm -f /usr/share/locale/de/LC_MESSAGES/cantara.mo
 rm -f /usr/share/locale/zh/LC_MESSAGES/cantara.mo
 
 rm -f /usr/bin/cantara
 
-xdg-desktop-menu uninstall reckel-cantara.desktop
+xdg-desktop-menu uninstall app.cantara.Cantara.desktop
