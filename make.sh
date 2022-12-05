b4f5aef8ee0db88bb5f7c2e733bfc21d69ad16c9
Jan Martin Reckel <jan-martin.reckel@s2017.tu-chemnitz.de>
2022-12-05 20:22:01 +0100

    Change Build Scripts, Adapt makefile

--- make.sh
+++ make.sh
@@ -3,20 +3,4 @@
 #
 
 lazbuild -B --bm="Release" --ws="qt5" src/Cantara.lpi
-
-if [ $? == 0 ]; then
-
-    mv src/cantara cantara
-    echo "Move src/cantara to cantara"
-
-    echo "Compilation succesfully done."
-
-    exit 0
-
-else
-
-    echo "Unable to build Cantara. There was an error while compiling. Check the previous logs."
-
-    echo 1
-
-fi
+mv src/cantara cantara
