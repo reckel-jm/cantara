86ac31f0177e9408c75eefdf4e7a03b98486fc90
haggen88 <68134588+haggen88@users.noreply.github.com>
2023-01-08 01:28:32 -0300

    Add Spanish

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ xdg-desktop-menu install app.cantara.Cantara.desktop
 install src/languages/de/cantara.mo /usr/share/locale/de/LC_MESSAGES/cantara.mo
 install src/languages/zh/cantara.mo /usr/share/locale/zh/LC_MESSAGES/cantara.mo
 install src/languages/it/cantara.mo /usr/share/locale/it/LC_MESSAGES/cantara.mo
+install src/languages/es/cantara.mo /usr/share/locale/es/LC_MESSAGES/cantara.mo
