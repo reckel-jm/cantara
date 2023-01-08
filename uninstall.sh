657297950be485d9ccd60f7272dd39ea2841fb06
haggen88 <68134588+haggen88@users.noreply.github.com>
2023-01-08 01:33:52 -0300

    update uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,7 @@ rm -f /usr/share/icons/app.cantara.Cantara.png
 rm -f /usr/share/locale/de/LC_MESSAGES/cantara.mo
 rm -f /usr/share/locale/zh/LC_MESSAGES/cantara.mo
 rm -f /usr/share/locale/it/LC_MESSAGES/cantara.mo
+rm -f /usr/share/locale/es/LC_MESSAGES/cantara.mo
 
 rm -f /usr/bin/cantara
 
