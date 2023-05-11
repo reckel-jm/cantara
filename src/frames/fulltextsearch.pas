95c005df13bb602f7e149451ae17db2d8e829a25
Jan Martin Reckel <jm.reckel@t-online.de>
2023-05-11 09:13:08 +0200

    Update Search Index so that the songs with its contents will be displayed

--- src/frames/fulltextsearch.pas
+++ src/frames/fulltextsearch.pas
@@ -5,7 +5,7 @@ unit fulltextsearch;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, StdCtrls, ExtCtrls, Dialogs, Lyrics, FGL;
+  Classes, SysUtils, Forms, Controls, StdCtrls, ExtCtrls, Dialogs, Lyrics, FGL, Types, LCLType, Graphics, Math;
 
 type
 
@@ -31,8 +31,9 @@ type
     LoadIndex: TPage;
     NoResult: TPage;
     SearchResults: TPage;
-    destructor Destroy; override;
     procedure EditSearchTermChange(Sender: TObject);
+    procedure ListBoxResultsDrawItem(Control: TWinControl; Index: Integer;
+      ARect: TRect; State: TOwnerDrawState);
     procedure NoResultBeforeShow(ASender: TObject; ANewPage: TPage;
       ANewIndex: Integer);
   private
@@ -40,7 +41,11 @@ type
   public
     IndexList: TIndexList;
     procedure CreateIndex;
+    destructor Destroy; override;
   end;
+
+ResourceString
+  StrSongTitle = 'Song Title';
 
 implementation
 
@@ -70,8 +75,7 @@ begin
     TempSong.importSongfile(RepoEntry.FilePath);
     IndexEntry.Song := TempSong;
     TempSong.output.Delimiter := LineEnding;
-    IndexEntry.ContentIndex:=Trim(LowerCase(TempSong.output.Text));
-    IndexEntry.ContentIndex:=StringReplace(IndexEntry.ContentIndex, LineEnding, ' ', [rfReplaceAll, rfIgnoreCase]);
+    IndexEntry.ContentIndex:=Trim(TempSong.output.Text);
     IndexList.Add(IndexEntry);
   end;
   // Change to the Notebook page with the Listbox
@@ -86,16 +90,55 @@ end;
 
 procedure TFrmFulltextsearch.EditSearchTermChange(Sender: TObject);
 var i: Integer;
+  ContentString, AddedContent: String;
+  PosInFileName, PosInContentIndex: Integer;
 begin
   Content.PageIndex:=1;
   ListboxResults.Clear;
   for i := 0 to IndexList.Count-1 do
   begin
-    if (Pos(LowerCase(EditSearchTerm.Text), LowerCase(IndexList.Items[i].Song.FileNameWithoutEnding)) > 0) or
-      (Pos(LowerCase(EditSearchTerm.Text), LowerCase(IndexList.Items[i].ContentIndex)) > 0) then
-       ListBoxResults.Items.Add(IndexList.Items[i].Song.FileNameWithoutEnding);
+    ContentString := IndexList.Items[i].ContentIndex;
+    PosInFileName := Pos(LowerCase(EditSearchTerm.Text), LowerCase(IndexList.Items[i].Song.FileNameWithoutEnding));
+    PosInContentIndex := Pos(LowerCase(EditSearchTerm.Text), LowerCase(ContentString));
+    if (PosInFileName > 0) or
+      (PosInContentIndex > 0) then
+       begin
+         AddedContent := IndexList.Items[i].Song.FileNameWithoutEnding;
+         AddedContent += LineEnding;
+         ContentString := IndexList.Items[i].ContentIndex;
+         if PosInContentIndex > 0 then
+           AddedContent += ContentString[PosInContentIndex-(Min(PosInContentIndex,60))+1..PosInContentIndex+Min(Length(ContentString)-PosInContentIndex, 60)-1]
+         else if PosInFileName > 0 then
+           AddedContent += StrSongTitle;
+         ListBoxResults.Items.Add(AddedContent);
+       end;
   end;
   if ListBoxResults.Count = 0 then Content.PageIndex := 2;
+end;
+
+procedure TFrmFulltextsearch.ListBoxResultsDrawItem(Control: TWinControl;
+  Index: Integer; ARect: TRect; State: TOwnerDrawState);
+var
+  aColor: TColor;
+  headingStyle: TTextStyle;
+  FontBaseHeight: Integer;
+  StringParts: TStringArray;
+begin
+  if odSelected in State then aColor := clActiveCaption
+  else aColor := clDefault;
+  FontBaseHeight := abs(GetFontData(Font.Reference.Handle).Height);
+  ListBoxResults.Canvas.Brush.Color := aColor;
+  ListBoxResults.Canvas.FillRect(aRect);
+  ListBoxResults.Canvas.Font.Bold:=True;
+  HeadingStyle.Alignment:=taCenter;
+  // Now we split the strings into the two parts
+  StringParts := ListBoxResults.Items[Index].Split(LineEnding);
+  ListBoxResults.Canvas.TextRect(ARect, 2, ARect.Top+2, StringParts[0], HeadingStyle);
+  if length(StringParts) > 1 then
+  begin
+    ListBoxResults.Canvas.Font.Bold:=False;
+    ListBoxResults.Canvas.TextRect(ARect, 2, ARect.Top+2+Round(FontBaseHeight*1.33), ListBoxResults.Items[Index]);
+  end;
 end;
 
 procedure TFrmFulltextsearch.NoResultBeforeShow(ASender: TObject;
