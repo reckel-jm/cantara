15e38bece59ca683e282e1b2c4103f6125b0ca66
Jan Martin Reckel <jm.reckel@t-online.de>
2023-09-30 18:51:14 +0200

    Add components to the new PresentationController, declare class functions,events and and properties

--- src/frames/presentationcontroller.pas
+++ src/frames/presentationcontroller.pas
@@ -5,31 +5,52 @@ unit presentationcontroller;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, ExtCtrls, ComCtrls, StdCtrls;
+  Classes, SysUtils, Forms, Controls, ExtCtrls, ComCtrls, StdCtrls, Slides;
 
 type
+  { TOnChangeSlide
+    The event which will be raised if a slide change has been commited by the user
+  }
+  TOnChangeSlide = procedure(NewSlideNumber: Integer) of object;
 
   { TFramePresentationController }
 
   TFramePresentationController = class(TFrame)
+    Button1: TButton;
     ButtonCancel: TButton;
     ButtonPrevious: TButton;
     ButtonNext: TButton;
     CurrentImage: TImage;
+    SongListBox: TListBox;
+    SongListBoxContainer: TGroupBox;
     PageControls: TPageControl;
     ButtonPanal: TPanel;
-    Splitter: TSplitter;
+    Splitter1: TSplitter;
+    VerticalSplitter: TSplitter;
     PageText: TTabSheet;
     TabSheet1: TTabSheet;
   private
-
+    procedure ChangeCurrentSlide(NewSlideNumber: Integer);
+    fCurrentSlide: Integer;
   public
+    property CurrentSlide: Integer read fCurrentSlide write ChangeCurrentSlide;
+    OnChangeSlide: TOnChangeSlide;
+    Slides: TSlideList;
 
   end;
 
 implementation
 
 {$R *.lfm}
+
+{ TFramePresentationController }
+
+procedure TFramePresentationController.ChangeCurrentSlide(
+  NewSlideNumber: Integer);
+begin
+  { To Add: The design changes }
+  Self.fCurrentSlide:=NewSlideNumber;
+end;
 
 end.
 
