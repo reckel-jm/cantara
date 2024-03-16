99fbbb3374c6d3fb1be44520c15cc16f4ff95d2a
Jan Martin Reckel <jm.reckel@t-online.de>
2024-03-16 16:16:00 +0100

    Switch Github automatisation to Qt6

--- make.sh
+++ make.sh
@@ -4,6 +4,6 @@
 
 cd src
 lazbuild -B --ws="qt5" bgrabitmap/bgrabitmap/bgrabitmappack.lpk
-lazbuild -B --bm="Release" --ws="qt5" Cantara.lpi
+lazbuild -B --bm="Release" --ws="qt6" Cantara.lpi
 cd ..
 mv src/cantara cantara
