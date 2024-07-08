e3f5fc9a003c037ff93d7efbeb21f2deecdbb356
Jan Martin Reckel <wukijm@gmail.com>
2024-07-08 16:23:25 +0200

    Update languages

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ install app.cantara.Cantara.png /usr/share/icons/app.cantara.Cantara.png
 
 xdg-desktop-menu install app.cantara.Cantara.desktop
 
-install src/languages/de/cantara.mo /usr/share/locale/de/LC_MESSAGES/cantara.mo
-install src/languages/zh/cantara.mo /usr/share/locale/zh/LC_MESSAGES/cantara.mo
-install src/languages/it/cantara.mo /usr/share/locale/it/LC_MESSAGES/cantara.mo
-install src/languages/es/cantara.mo /usr/share/locale/es/LC_MESSAGES/cantara.mo
-install src/languages/nl/cantara.mo /usr/share/locale/nl/LC_MESSAGES/cantara.mo
+for lang in de zh it es nl
+do
+    install src/languages/$lang/cantara.mo /usr/share/locale/$lang/LC_MESSAGES/cantara.mo
+done
