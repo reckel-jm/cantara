d85f871bc81cb8fe70b8ad64046d575e59adfd0b
Jan Martin Reckel <wukijm@gmail.com>
2024-07-20 12:51:35 +0200

    Update Makefile

--- make.sh
+++ make.sh
@@ -4,6 +4,7 @@
 
 cd src
 lazbuild -B --ws="qt6" bgrabitmap/bgrabitmap/bgrabitmappack.lpk
+lazbuild -B --ws="qt6" metadarkstyle/metadarkstyle.lpk
 lazbuild -B --bm="Release" --ws="qt6" Cantara.lpi
 cd ..
 mv src/cantara cantara
